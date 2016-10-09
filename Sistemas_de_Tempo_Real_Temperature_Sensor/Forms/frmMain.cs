f193794631905928df94089b3f815a113ea3fd0f
edipof <edipofernandes7@gmail.com>
2016-10-09 20:35:15 -0300

    Teste Invoke

--- Sistemas_de_Tempo_Real_Temperature_Sensor/Forms/frmMain.cs
+++ Sistemas_de_Tempo_Real_Temperature_Sensor/Forms/frmMain.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Sistemas_de_Tempo_Real_Temperature_Sensor.Classes;
+using System.Threading;
+using static Sistemas_de_Tempo_Real_Temperature_Sensor.Forms.ucTemperatureSensor;
 
 namespace Sistemas_de_Tempo_Real_Temperature_Sensor.Forms
 {
@@ -18,7 +20,8 @@ namespace Sistemas_de_Tempo_Real_Temperature_Sensor.Forms
         private CyclicExecutive m_CyclicExecutive = new CyclicExecutive();
         private ucTemperatureSensor m_ucTemperatureSensor = new ucTemperatureSensor();
   
-        public delegate void TemperatureSensorEventHandler();
+        
+        
         #endregion
 
         #region Constructor
@@ -27,14 +30,42 @@ namespace Sistemas_de_Tempo_Real_Temperature_Sensor.Forms
         {
             InitializeComponent();
             m_TemperaturePanel.Controls.Add(m_ucTemperatureSensor);
+
+            Thread m_ThreadSensor = new Thread(atualizaThreadSensor);
+            m_ThreadSensor.Start();
+
         }
 
         #endregion
 
         #region Events
+
+        private void atualizaThreadSensor()
+        {
+            int count = 0;
+            while (true)
+            {
+                count++;
+                Thread.Sleep(1000);
+                if (m_ucTemperatureSensor.InvokeRequired)
+                {
+                    this.Invoke(new TemperatureSensorEventHandler(setLabel1), new object[] { count.ToString() + " ° C" });
+                }
+                else
+                {
+                    m_ucTemperatureSensor.TemperatureLabel = count.ToString();
+                }
+            }
+        }
+
         #endregion
 
         #region Methods
+
+        private void setLabel1(string e)
+        {
+            m_ucTemperatureSensor.TemperatureLabel = e;
+        }
         #endregion
 
     }
--- Sistemas_de_Tempo_Real_Temperature_Sensor/Forms/ucTemperatureSensor.cs
+++ Sistemas_de_Tempo_Real_Temperature_Sensor/Forms/ucTemperatureSensor.cs
@@ -12,15 +12,32 @@ namespace Sistemas_de_Tempo_Real_Temperature_Sensor.Forms
 {
     public partial class ucTemperatureSensor : UserControl
     {
+        public delegate void TemperatureSensorEventHandler(string text);
+
+
+        public string TemperatureLabel
+        {
+            get
+            {
+                return m_LblTemperature.Text;
+            }
+
+            set
+            {
+                m_LblTemperature.Text = value;
+            }
+        }
+
         public ucTemperatureSensor()
         {
             InitializeComponent();
-            m_LblTemperature.Text = "38° C";
+            m_LblTemperature.Text = "0° C";
         }
 
         private void ucTemperatureSensor_Load(object sender, EventArgs e)
         {
 
         }
+
     }
 }
