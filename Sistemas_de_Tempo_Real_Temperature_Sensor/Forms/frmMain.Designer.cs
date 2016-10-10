4af24af9b2bb4b5c7c95627bb266951a46132445
edipof <edipofernandes7@gmail.com>
2016-10-10 18:07:01 -0300

    Alterações no designer

--- Sistemas_de_Tempo_Real_Temperature_Sensor/Forms/frmMain.Designer.cs
+++ Sistemas_de_Tempo_Real_Temperature_Sensor/Forms/frmMain.Designer.cs
@@ -29,29 +29,148 @@
         private void InitializeComponent()
         {
             this.m_TemperaturePanel = new System.Windows.Forms.Panel();
+            this.m_ProductLinePanel = new System.Windows.Forms.Panel();
+            this.m_PressionPanel = new System.Windows.Forms.Panel();
+            this.m_BbxControls = new System.Windows.Forms.GroupBox();
+            this.m_lblPressaoGas = new System.Windows.Forms.Label();
+            this.m_lblTemperature = new System.Windows.Forms.Label();
+            this.m_lblLinhaProducao = new System.Windows.Forms.Label();
+            this.m_BtnGasUp = new System.Windows.Forms.Button();
+            this.m_BtnGasDown = new System.Windows.Forms.Button();
+            this.m_BtnDesligar = new System.Windows.Forms.Button();
+            this.m_BtnIniciar = new System.Windows.Forms.Button();
+            this.m_BbxControls.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_TemperaturePanel
             // 
             this.m_TemperaturePanel.Location = new System.Drawing.Point(1, 2);
             this.m_TemperaturePanel.Name = "m_TemperaturePanel";
-            this.m_TemperaturePanel.Size = new System.Drawing.Size(361, 202);
+            this.m_TemperaturePanel.Size = new System.Drawing.Size(334, 177);
             this.m_TemperaturePanel.TabIndex = 0;
+            // 
+            // m_ProductLinePanel
+            // 
+            this.m_ProductLinePanel.Location = new System.Drawing.Point(494, 2);
+            this.m_ProductLinePanel.Name = "m_ProductLinePanel";
+            this.m_ProductLinePanel.Size = new System.Drawing.Size(432, 177);
+            this.m_ProductLinePanel.TabIndex = 1;
+            // 
+            // m_PressionPanel
+            // 
+            this.m_PressionPanel.Location = new System.Drawing.Point(1, 210);
+            this.m_PressionPanel.Name = "m_PressionPanel";
+            this.m_PressionPanel.Size = new System.Drawing.Size(334, 166);
+            this.m_PressionPanel.TabIndex = 1;
+            // 
+            // m_BbxControls
+            // 
+            this.m_BbxControls.Controls.Add(this.m_BtnGasDown);
+            this.m_BbxControls.Controls.Add(this.m_BtnGasUp);
+            this.m_BbxControls.Location = new System.Drawing.Point(351, 210);
+            this.m_BbxControls.Name = "m_BbxControls";
+            this.m_BbxControls.Size = new System.Drawing.Size(133, 166);
+            this.m_BbxControls.TabIndex = 2;
+            this.m_BbxControls.TabStop = false;
+            this.m_BbxControls.Text = "Controle de Gás";
+            // 
+            // m_lblPressaoGas
+            // 
+            this.m_lblPressaoGas.AutoSize = true;
+            this.m_lblPressaoGas.Location = new System.Drawing.Point(133, 383);
+            this.m_lblPressaoGas.Name = "m_lblPressaoGas";
+            this.m_lblPressaoGas.Size = new System.Drawing.Size(67, 13);
+            this.m_lblPressaoGas.TabIndex = 3;
+            this.m_lblPressaoGas.Text = "Pressão Gás";
+            // 
+            // m_lblTemperature
+            // 
+            this.m_lblTemperature.AutoSize = true;
+            this.m_lblTemperature.Location = new System.Drawing.Point(133, 186);
+            this.m_lblTemperature.Name = "m_lblTemperature";
+            this.m_lblTemperature.Size = new System.Drawing.Size(67, 13);
+            this.m_lblTemperature.TabIndex = 4;
+            this.m_lblTemperature.Text = "Temperatura";
+            // 
+            // m_lblLinhaProducao
+            // 
+            this.m_lblLinhaProducao.AutoSize = true;
+            this.m_lblLinhaProducao.Location = new System.Drawing.Point(678, 186);
+            this.m_lblLinhaProducao.Name = "m_lblLinhaProducao";
+            this.m_lblLinhaProducao.Size = new System.Drawing.Size(97, 13);
+            this.m_lblLinhaProducao.TabIndex = 5;
+            this.m_lblLinhaProducao.Text = "Linha de Produção";
+            // 
+            // m_BtnGasUp
+            // 
+            this.m_BtnGasUp.Location = new System.Drawing.Point(6, 33);
+            this.m_BtnGasUp.Name = "m_BtnGasUp";
+            this.m_BtnGasUp.Size = new System.Drawing.Size(121, 45);
+            this.m_BtnGasUp.TabIndex = 0;
+            this.m_BtnGasUp.Text = "+ Pressão";
+            this.m_BtnGasUp.UseVisualStyleBackColor = true;
+            // 
+            // m_BtnGasDown
+            // 
+            this.m_BtnGasDown.Location = new System.Drawing.Point(6, 96);
+            this.m_BtnGasDown.Name = "m_BtnGasDown";
+            this.m_BtnGasDown.Size = new System.Drawing.Size(121, 45);
+            this.m_BtnGasDown.TabIndex = 1;
+            this.m_BtnGasDown.Text = "- Pressão";
+            this.m_BtnGasDown.UseVisualStyleBackColor = true;
+            // 
+            // m_BtnDesligar
+            // 
+            this.m_BtnDesligar.Location = new System.Drawing.Point(805, 331);
+            this.m_BtnDesligar.Name = "m_BtnDesligar";
+            this.m_BtnDesligar.Size = new System.Drawing.Size(121, 45);
+            this.m_BtnDesligar.TabIndex = 6;
+            this.m_BtnDesligar.Text = "Desligar";
+            this.m_BtnDesligar.UseVisualStyleBackColor = true;
+            // 
+            // m_BtnIniciar
+            // 
+            this.m_BtnIniciar.Location = new System.Drawing.Point(666, 331);
+            this.m_BtnIniciar.Name = "m_BtnIniciar";
+            this.m_BtnIniciar.Size = new System.Drawing.Size(121, 45);
+            this.m_BtnIniciar.TabIndex = 7;
+            this.m_BtnIniciar.Text = "Iniciar";
+            this.m_BtnIniciar.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(938, 400);
+            this.Controls.Add(this.m_BtnIniciar);
+            this.Controls.Add(this.m_BtnDesligar);
+            this.Controls.Add(this.m_lblLinhaProducao);
+            this.Controls.Add(this.m_lblTemperature);
+            this.Controls.Add(this.m_lblPressaoGas);
+            this.Controls.Add(this.m_BbxControls);
+            this.Controls.Add(this.m_PressionPanel);
+            this.Controls.Add(this.m_ProductLinePanel);
             this.Controls.Add(this.m_TemperaturePanel);
             this.Name = "frmMain";
             this.Text = "frmMain";
+            this.m_BbxControls.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel m_TemperaturePanel;
+        private System.Windows.Forms.Panel m_ProductLinePanel;
+        private System.Windows.Forms.Panel m_PressionPanel;
+        private System.Windows.Forms.GroupBox m_BbxControls;
+        private System.Windows.Forms.Label m_lblPressaoGas;
+        private System.Windows.Forms.Label m_lblTemperature;
+        private System.Windows.Forms.Label m_lblLinhaProducao;
+        private System.Windows.Forms.Button m_BtnGasDown;
+        private System.Windows.Forms.Button m_BtnGasUp;
+        private System.Windows.Forms.Button m_BtnDesligar;
+        private System.Windows.Forms.Button m_BtnIniciar;
     }
 }
